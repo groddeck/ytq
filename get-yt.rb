8f45ed75390fcb4664fa182c403ac0b10230aa6c
Rob Williams <robwilliams42@gmail.com>
2014-09-10 22:37:27 -0400

    bugzid: 10000 add error handling

--- get-yt.rb
+++ get-yt.rb
@@ -10,7 +10,13 @@ get '/play/:yt_id' do
     `say Thanks for the selection, #{params[:who]}`
   end
   puts `youtube-dl -x -t --audio-format 'mp3' #{params[:yt_id]}`
+  unless $?.success?
+  	`say Error downloading video`
+  end
   # `say thanks for suggestion!`
   `find . -name *#{params[:yt_id]}.mp3 | xargs -I arg open "arg"`
+  unless $?.success?
+  	`say Error playing song`
+  end
   # `say alright everyone time to choose another song`
 end
