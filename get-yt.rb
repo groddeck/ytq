d6efa27e08ff3e2728b677156fd993eaafd495b5
groddeck <jose.c.rivera@gmail.com>
2014-09-10 22:15:57 -0400

    add who param

--- get-yt.rb
+++ get-yt.rb
@@ -6,6 +6,11 @@ end
 
 get '/play/:yt_id' do
   puts "cool, cool, you want to play #{params[:yt_id]}"
+  if params[:who]
+    `say Thanks for the selection, #{params[:who]}`
+  end
   puts `youtube-dl -x -t --audio-format 'mp3' #{params[:yt_id]}`
+  # `say thanks for suggestion!`
   `find . -name *#{params[:yt_id]}.mp3 | xargs -I arg open "arg"`
+  # `say alright everyone time to choose another song`
 end
