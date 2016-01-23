30700d9f9f207134c5a9f2458cef99524859b8ed
groddeck <jose.c.rivera@gmail.com>
2016-01-22 20:55:06 -0500

    keep list of downloaded files

--- jobs/audio_play_job.rb
+++ jobs/audio_play_job.rb
@@ -2,7 +2,7 @@ class AudioPlayJob
   @queue = :playlist
 
   def self.perform(youtube_id, who=nil)
-    `find . -name *#{youtube_id}.mp3 | xargs -I arg afplay "arg"`
+    `find . -name #{youtube_id}.mp3 | xargs -I arg afplay "arg"`
     unless $?.success?
     	`say Error playing song`
     end
