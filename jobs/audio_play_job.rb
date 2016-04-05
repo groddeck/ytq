a51faf510411a12e96a8853deca0ecd00aee89db
groddeck <jose.c.rivera@gmail.com>
2016-04-04 21:10:08 -0400

    search function

--- jobs/audio_play_job.rb
+++ jobs/audio_play_job.rb
@@ -1,7 +1,7 @@
 class AudioPlayJob
   @queue = :playlist
 
-  def self.perform(youtube_id, who=nil)
+  def self.perform(youtube_id, who=nil, name)
     `find . -name #{youtube_id}.mp3 | xargs -I arg afplay "arg"`
     unless $?.success?
     	`say Error playing song`
